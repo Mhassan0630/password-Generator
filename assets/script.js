166858512b9b433943b6987ede3e34d0960ff605
MUNTAAS <98139114+Mhassan0630@users.noreply.github.com>
2023-05-11 21:35:01 -0500

    modified script.js/Readme.md

--- assets/script.js
+++ assets/script.js
@@ -3,20 +3,13 @@ var choiceArr = [];
 
 var specialCharArr = ['!', '@', '#', '$', '%', '^', '&', '*','(',')','[',']','{','}','|','?','/','<','>'];
 var lowerCaseArr = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z',];
-var upperCaseArr = ['A','B','C ','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z',];
+var upperCaseArr = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z',];
 var numberArr = ['1','2','3','4','5','6','7','8','9','0',];
 
-
-
-
-// Assignment Code 
 var generateBtn = document.querySelector("#generate");
 
-
-// Add event listener too generate button
 generateBtn.addEventListener("click", writePassword);
 
-// Write password to the #password input
 function writePassword() {
   var correctPrompts = getPrompts();  //either true or false
   var passwordText = document.querySelector("#password");
@@ -25,13 +18,11 @@ function writePassword() {
       var newPassword = generatePassword();
       passwordText.value = newPassword;
     } else {
-      passwordTest.value = "";
+      passwordText.value = "";
     }
-
 }
 
 function generatePassword() {
-  // I would generatePassword based on the prompts
   var password = "";
   for(var i = 0; i < characterLength; i++) {
       var randomIndex = Math.floor(Math.random() * choiceArr.length);
@@ -44,11 +35,13 @@ function getPrompts(){
   choiceArr = [];
   characterLength = parseInt(prompt("How many characters do you want your password to be? (8 - 128 characters")); //NaN
 
-  if(isNaN(characterLength) || characterLength < 8 || characterLength > 128) {  //I'm hoping that this is all false
+  if(isNaN(characterLength) || characterLength < 8 || characterLength > 128) {  
   alert("character length has to be a number, 8 - 128 digits. Please try again.");
+  return false;
 }
 
 if (confirm("Would you like lowercase letters in your password?")) {
+  choiceArr = choiceArr.concat(lowerCaseArr);
 }
 if (confirm("Would you like uppercase letters in your password?")) {
   choiceArr = choiceArr.concat(upperCaseArr);
@@ -59,5 +52,11 @@ if (confirm("Would you like special characters in your password?")) {
 if (confirm("Would you like numbers in your password?")) {
   choiceArr = choiceArr.concat(numberArr);
 }
+
+if(choiceArr.length === 0){
+  alert("At least one character type should be selected. Please try again.");
+  return false;
+}
+
 return true;
 }
